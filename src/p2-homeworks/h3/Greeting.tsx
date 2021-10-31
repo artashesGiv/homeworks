3a682bf1937e1d3b2026424436fcc3f12b3cfa73
Artashes Dzhivanian <artashes.giv@gmail.com>
2021-10-31 14:51:20 +0300

    hw3

--- src/p2-homeworks/h3/Greeting.tsx
+++ src/p2-homeworks/h3/Greeting.tsx
@@ -1,28 +1,29 @@
-import React from 'react'
-import s from './Greeting.module.css'
+import React, {ChangeEvent, KeyboardEvent} from 'react'
+import s from './Greeting.module.scss'
 
 type GreetingPropsType = {
-    name: any // need to fix any
-    setNameCallback: any // need to fix any
-    addUser: any // need to fix any
-    error: any // need to fix any
-    totalUsers: any // need to fix any
+   name: string
+   setNameCallback: (e: ChangeEvent<HTMLInputElement>) => void
+   onKeyPressInput: (e: KeyboardEvent<HTMLInputElement>) => void
+   addUser: () => void
+   error: string
+   totalUsers: number
 }
 
 // презентационная компонента (для верстальщика)
 const Greeting: React.FC<GreetingPropsType> = (
-    {name, setNameCallback, addUser, error, totalUsers} // деструктуризация пропсов
+   {name, setNameCallback, addUser, error, totalUsers, onKeyPressInput} // деструктуризация пропсов
 ) => {
-    const inputClass = s.error // need to fix with (?:)
+   const inputClass = error === 'Error' ? s.error : s.notError
 
-    return (
-        <div>
-            <input value={name} onChange={setNameCallback} className={inputClass}/>
-            <span>{error}</span>
-            <button onClick={addUser}>add</button>
-            <span>{totalUsers}</span>
-        </div>
-    )
+   return (
+      <div className={s.someClass}>
+         <input value={name} onChange={setNameCallback} className={inputClass} onKeyPress={onKeyPressInput}/>
+         <span>{error}</span>
+         <button onClick={addUser} className={s.button}>Add</button>
+         <span>{totalUsers}</span>
+      </div>
+   )
 }
 
 export default Greeting
--- src/p2-homeworks/h3/GreetingContainer.tsx
+++ src/p2-homeworks/h3/GreetingContainer.tsx
@@ -1,9 +1,10 @@
-import React, {useState} from 'react'
+import React, {ChangeEvent, KeyboardEvent, useState} from 'react'
 import Greeting from './Greeting'
+import {UserType} from './HW3'
 
 type GreetingContainerPropsType = {
-    users: any // need to fix any
-    addUserCallback: any // need to fix any
+   users: Array<UserType>
+   addUserCallback: (name: string) => void
 }
 
 // более простой и понятный для новичков
@@ -11,28 +12,37 @@ type GreetingContainerPropsType = {
 
 // более современный и удобный для про :)
 // уровень локальной логики
-const GreetingContainer: React.FC<GreetingContainerPropsType> = ({users, addUserCallback}) => { // деструктуризация пропсов
-    const [name, setName] = useState<any>('') // need to fix any
-    const [error, setError] = useState<any>('') // need to fix any
+const GreetingContainer: React.FC<GreetingContainerPropsType> = ({users, addUserCallback}) => {
+   const [name, setName] = useState<string>('')
+   const [error, setError] = useState<string>('')
 
-    const setNameCallback = (e: any) => { // need to fix any
-        setName('') // need to fix
-    }
-    const addUser = () => {
-        alert(`Hello  !`) // need to fix
-    }
+   const setNameCallback = (e: ChangeEvent<HTMLInputElement>) => setName(e.currentTarget.value)
+   const onKeyPressInput = (e: KeyboardEvent<HTMLInputElement>) => e.key === 'Enter' ? addUser() : false
 
-    const totalUsers = 0 // need to fix
+   const addUser = () => {
+      let trimName = name.trim()
+      if (trimName !== '') {
+         addUserCallback(trimName)
+         alert(`Hello ${trimName}!`)
+         setName('')
+         setError('')
+      } else {
+         setError('Error')
+      }
+   }
 
-    return (
-        <Greeting
-            name={name}
-            setNameCallback={setNameCallback}
-            addUser={addUser}
-            error={error}
-            totalUsers={totalUsers}
-        />
-    )
+   const totalUsers = users.length
+
+   return (
+      <Greeting
+         name={name}
+         setNameCallback={setNameCallback}
+         onKeyPressInput={onKeyPressInput}
+         addUser={addUser}
+         error={error}
+         totalUsers={totalUsers}
+      />
+   )
 }
 
 export default GreetingContainer
